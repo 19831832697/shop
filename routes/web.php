6c7fc72dd536af1d2fbcf9ecacabf769d9d8f356
zhangchenyang <zhangchenyang@localhost.localdomain>
2019-05-23 19:19:37 +0800

    web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@
 //});
 
 Route::get('/','Home\HomeController@index');
+
+//注册页面
+Route::get('register','User\UserController@register');
+//注册
+Route::post('registerDo','User\UserController@registerDo');
+//登录页面
+Route::get('login','User\UserController@login');
+//登录
+Route::post('loginDo','User\UserController@loginDo');
+
+//商品详情
+Route::get('/goods/goodslist','Goods\GoodsController@goodslist');
+//购物车
+Route::get('/cart','Cart\CartController@cart');
+//生成订单
+Route::post('pay','pay\PayController@pay');
+//订单列表
+Route::post('payShow','pay\PayController@payShow');
