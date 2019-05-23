2b037fa1a356af712b6f2da177969424ac4d54ab
Your Name <lianshijie@3023668879.com>
2019-05-23 15:18:05 +0800

    model

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers\Home;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Model\GoodsModel;
 class HomeController extends Controller
 {
     //
     public function index(){
 //        echo "aa";
         return view('home.index');
+    }
+    //最新商品
+    public function goodsnew(){
+        $where=[
+            'goods_status'=>1,
+            'goods_new'=>1
+        ];
+        $goodsInfo=GoodsModel::where($where)->all();
+        dd($goodsInfo);
     }
 }
