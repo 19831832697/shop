9a1322e866c09241c0b2a4c67e35759e1785af8f
Your Name <lianshijie@3023668879.com>
2019-05-24 09:59:48 +0800

    debu

--- app/Http/Controllers/Collect/ColController.php
+++ app/Http/Controllers/Collect/ColController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\Collect;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use App\Model\PraiseModel;
 class ColController extends Controller
 {
     //
-    public function collect(){
+    public function add(Request $request){
+
         return view();
     }
 }
