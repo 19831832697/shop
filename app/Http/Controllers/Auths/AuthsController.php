450e8d78480974c32fa90c3daac84d3f33ef8710
cuifang <3152185832@qq.com>
2019-05-27 07:22:15 +0000

    Wx

--- app/Http/Controllers/Auths/AuthsController.php
+++ app/Http/Controllers/Auths/AuthsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\DB;
+use App\Model\Wx_user;
 class AuthsController extends Controller
 {
     public function wxauth(){
@@ -26,11 +27,17 @@ class AuthsController extends Controller
             'sex'=>$urlinfo['sex'],
             'headimgurl'=>$urlinfo['headimgurl'],
         ];
-        $ser=DB::table('wx_user')->insert($info);
-        if($res){
-            echo "<script>alert('登录成功');location.href='/';</script>";
+        $acc=DB::table('wx_user')->where(['openid'=>$openid])->first();
+        if($acc){
+            echo "<script>alert('欢迎回来');location.href='/login';</script>";
         }else{
-            echo "<script>alert('微信授权失败');location.href='/login';</script>";
+            $res=Wx_user::insertId($info);
+            dd($res);
+            if($res){
+                echo "<script>alert('登录成功');location.href='/';</script>";
+            }else{
+                echo "<script>alert('微信授权失败');location.href='/login';</script>";
+            }
         }
     }
     //getcurl
