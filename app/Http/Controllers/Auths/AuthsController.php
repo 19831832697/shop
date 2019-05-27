3af87001d6d40621a2454c6d204d146afd316990
cuifang <3152185832@qq.com>
2019-05-27 02:22:53 +0000

    微信授权

--- app/Http/Controllers/Auths/AuthsController.php
+++ app/Http/Controllers/Auths/AuthsController.php
@@ -19,8 +19,18 @@ class AuthsController extends Controller
         $secret=env('WX_APPSECRETl');
         $code=$_GET['code'];
         $token="https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
-        echo "获取token";print_r($token);echo "<br>";
-        $access_token="https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=$appid&grant_type=refresh_token&refresh_token=$token";
-        echo "获取access_token";print_r($access_token);echo "<br>";
+        echo $this->getcurl($token);die;
+//        echo "获取token";print_r($token);echo "<br>";
+//        $access_token="https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=$appid&grant_type=refresh_token&refresh_token=$token";
+//        echo "获取access_token";print_r($access_token);echo "<br>";
+    }
+    //curl
+    public function getcurl($url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        echo $output;
+        curl_close($ch);
     }
 }
